a777acdc0945ec9d197004c22cc7038cf3a3db4f
Vinnie <vinnie.scalco@gmail.com>
2018-04-10 15:09:40 -0500

    Add Bookmarks app

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class bookmarks(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=1000)
+    url = models.URLField()
+
+    class Meta:
+        unique_together = (("user", "name"),("url", "user") )
+    
+    def __unicode__(self):
+        return self.url
