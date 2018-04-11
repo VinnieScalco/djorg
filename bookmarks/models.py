4b1e4141361ef0fc0eebd2f68c616fa25ce9771c
Vinnie <vinnie.scalco@gmail.com>
2018-04-11 12:50:33 -0500

    Classes

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,14 @@
+from uuid import uuid4
 from django.db import models
 
-class bookmarks(models.Model):
-	name = models.CharField(max_length = 1000)
-	url = models.URLField()
+# Create your models here.
+class Bookmark(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+	name = models.CharField(max_length = 500, unique=True)
+	notes = models.TextField('Notes', blank=True)
+	url = models.URLField('URL', unique=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	last_modified = models.DateTimeField(auto_now=True)
+
+class FilmBookmark(Bookmark):
+	name = models.CharField(max_length=500, unique=True)
