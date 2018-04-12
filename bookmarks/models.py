79653e52590b2488e08f9fa3bf181475cc29ab1a
Vinnie Scalco <vinnie.scalco@gmail.com>
2018-04-12 18:44:25 -0500

    Added Category Drop-down

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Bookmark(models.Model):
-    FILM = 'Fi'
-    MUSIC = 'Mu'
-    TECH = 'Te'
-    GAME = 'Ga'
+    FILM = 'FI'
+    GAME = 'GA'
+    MUSIC = 'MU'
+    TECH = 'TE'
     TYPE_OF_BOOKMARK_CHOICES = (
         (FILM, 'Film'),
+        (GAME, 'Gaming'),
         (MUSIC, 'Music'),
         (TECH, 'Tech'),
-        (GAME, 'Gaming')
     )
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField('Name', max_length=200)
@@ -19,9 +19,9 @@ class Bookmark(models.Model):
     url = models.URLField('URL', unique=True)
     category = models.CharField(
         'Category',
-        max_length=100,
-        choices =TYPE_OF_BOOKMARK_CHOICES,
-        blank=True, 
+        max_length=50,
+        choices=TYPE_OF_BOOKMARK_CHOICES,
+        blank=True,
         null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
@@ -32,17 +32,3 @@ class Bookmark(models.Model):
 
     def __unicode__(self):
         return self.name
-
-# class FilmBookmark(Bookmark):
-#         name = models.CharField(max_length=200)
-#         notes = models.TextField('Notes', blank=True)
-#         url = models.URLField('URL', unique=True)
-#         created_at = models.DateTimeField(auto_now_add=True)
-#         last_modified = models.DateTimeField(auto_now=True)
-
-# class GameBookmark(Bookmark):
-#         name = models.CharField(max_length=200)
-#         notes = models.TextField('Notes', blank=True)
-#         url = models.URLField('URL', unique=True)
-#         created_at = models.DateTimeField(auto_now_add=True)
-#         last_modified = models.DateTimeField(auto_now=True)
