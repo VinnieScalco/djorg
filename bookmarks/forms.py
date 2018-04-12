03ce0f929dac05c546a11dde252e52f576a71d41
Vinnie Scalco <vinnie.scalco@gmail.com>
2018-04-12 17:55:41 -0500

    Fun with Addtl Fields

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -5,4 +5,4 @@ from .models import Bookmark
 class BookmarkForm(forms.ModelForm):
     class Meta:
         model = Bookmark
-        fields = ('URL', 'Name', 'Notes')
+        fields = ('url', 'name', 'notes', 'category')
--- bookmarks/models.py
+++ bookmarks/models.py
@@ -3,10 +3,21 @@ from django.db import models
 
 # Create your models here.
 class Bookmark(models.Model):
+    FILM = 'Fi'
+    MUSIC = 'Mu'
+    TECH = 'Te'
+    GAME = 'Ga'
+    TYPE_OF_BOOKMARK_CHOICES = (
+        (FILM, 'Film'),
+        (MUSIC, 'Music'),
+        (TECH, 'Tech'),
+        (GAME, 'Gaming')
+    )
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField('Name', max_length=200)
     notes = models.TextField('Notes', blank=True)
     url = models.URLField('URL', unique=True)
+    category = models.CharField('Category', blank=True, null=True, max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
