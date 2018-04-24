cff8390767afdfc092b9b05d91b7fb18d7bb6f87
Vinnie Scalco <vinnie.scalco@gmail.com>
2018-04-24 12:37:16 -0500

    added Tests

--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Bookmark
 
 # Create your tests here.
+class BookmarkTestCase(TestCase):
+    def setUp(self):
+        Bookmark.objects.create(name="Noteless bookmark", 
+                                url="http://www.bookmarkless-in-seattle.com")
+        Bookmark.objects.create(name="More note-worthy bookmark",
+                                note="This bookmark is worth writing a note about!",
+                                url="http://www.note-this.com")
+        pass
+
+    def test_retrieving_valid_bookmark(self):
+        """Test that a stored bookmark has correct values."""
+        noteless_bookmark = Bookmark.objects.get("Noteless bookmark")
+        self.assertEqual(noteless_bookmark.name, "Noteless bookmark")
+        self.assertEqual(noteless_bookmark.url, "http://www.bookmarkless-in-seattle.com")
+        self.assertEqual(noteless_bookmark.note, "")
+        noted_bookmark = Bookmark.objects.get("More note-worthy bookmark")
+        self.assertEqual(noted_bookmark.name, "More note-worthy bookmark")
+        self.assertEqual(noted_bookmark.url, "http://www.note-this.com")
+        self.assertEqual(noted_bookmark.note, "This bookmark is worth writing a note about!")
