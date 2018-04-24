79e6a278ab841447f451a0ae556068d50afc48af
Vinnie Scalco <vinnie.scalco@gmail.com>
2018-04-24 12:42:52 -0500

    added Tests

--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -7,17 +7,17 @@ class BookmarkTestCase(TestCase):
         Bookmark.objects.create(name="Noteless bookmark", 
                                 url="http://www.bookmarkless-in-seattle.com")
         Bookmark.objects.create(name="More note-worthy bookmark",
-                                note="This bookmark is worth writing a note about!",
+                                notes="This bookmark is worth writing a note about!",
                                 url="http://www.note-this.com")
         pass
 
     def test_retrieving_valid_bookmark(self):
         """Test that a stored bookmark has correct values."""
-        noteless_bookmark = Bookmark.objects.get("Noteless bookmark")
+        noteless_bookmark = Bookmark.objects.get(name="Noteless bookmark")
         self.assertEqual(noteless_bookmark.name, "Noteless bookmark")
         self.assertEqual(noteless_bookmark.url, "http://www.bookmarkless-in-seattle.com")
-        self.assertEqual(noteless_bookmark.note, "")
-        noted_bookmark = Bookmark.objects.get("More note-worthy bookmark")
+        self.assertEqual(noteless_bookmark.notes, "")
+        noted_bookmark = Bookmark.objects.get(name="More note-worthy bookmark")
         self.assertEqual(noted_bookmark.name, "More note-worthy bookmark")
         self.assertEqual(noted_bookmark.url, "http://www.note-this.com")
-        self.assertEqual(noted_bookmark.note, "This bookmark is worth writing a note about!")
+        self.assertEqual(noted_bookmark.notes, "This bookmark is worth writing a note about!")
